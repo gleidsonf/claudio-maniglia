076a6277e0a6248780b6c18d2167c3e4150eb7fb
Gleidson Silva <gleidson.fgs@gmail.com>
2017-08-14 16:38:06 -0300

    Front end base

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from .models import Post
 from .models import Type
 
+def base(request):
+    tags = Type.objects.all()
+    return render(request, 'blog/base.html', {'tags': tags})
 def post_list(request):
     tags = Type.objects.all()
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
