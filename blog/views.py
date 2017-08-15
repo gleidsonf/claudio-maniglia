193d2da0c0d207ca6585510b78da8be643551d30
Gleidson Silva <gleidson.fgs@gmail.com>
2017-08-15 18:14:21 -0300

    Alterando front

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .models import Type
@@ -6,7 +6,15 @@ from .models import Type
 def base(request):
     tags = Type.objects.all()
     return render(request, 'blog/base.html', {'tags': tags})
+
+def body(request):
+    return render(request, 'blog/body.html', {})
+
 def post_list(request):
     tags = Type.objects.all()
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts, 'tags': tags})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
