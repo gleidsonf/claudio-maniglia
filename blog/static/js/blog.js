8124d3eff00da6681a1b096ead98fb5c68327d9e
acensjr <projetos.acens@gmail.com>
2017-08-16 11:23:22 -0300

    Alterando cores e navegacao suave

--- blog/static/js/blog.js
+++ blog/static/js/blog.js
@@ -1,38 +1,7 @@
-(function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#main-nav',
-    offset: 54
-  });
-
-  // Closes responsive menu when a link is clicked
-  $('.navbar-collapse>ul>li>a').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(function() {
-    if ($("#main-nav").offset().top > 100) {
-      $("#main-nav").addClass("navbar-shrink");
-    } else {
-      $("#main-nav").removeClass("navbar-shrink");
-    }
-  });
-
-})(jQuery); // End of use strict
+var $doc = $('html, body');
+$('a').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
