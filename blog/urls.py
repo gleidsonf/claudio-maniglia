e498040fabef9fa227229f28fe703fce326ff783
Gleidson Silva <gleidson.fgs@gmail.com>
2017-08-18 00:37:51 -0300

    Implementando post detail e filtro

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^$', views.post_list, name='root_url'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post_by_type/(?P<pk>[0-9]+)/$', views.post_by_type, name='post_by_type'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.utils import timezone
 from .models import Post
 from .models import Type
@@ -6,6 +7,10 @@ from .models import Type
 def base(request):
     tags = Type.objects.all()
     return render(request, 'blog/base.html', {'tags': tags})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def body(request):
     return render(request, 'blog/body.html', {})
@@ -15,6 +20,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts, 'tags': tags})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+def post_by_type(request, pk):
+    type = Type.objects.get(pk=pk)
+    posts = get_list_or_404(Post, type_post=type)
+    return render(request, 'blog/post_list.html', {'posts': posts })
