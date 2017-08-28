f495de3194e77663ada381462e18a7c1fc5f88a3
Mario Fonteles <fontelesjr15@gmail.com>
2017-08-28 10:40:34 +0500

    Formulario de contato com envio de email

--- blog/models.py
+++ blog/models.py
@@ -43,3 +43,6 @@ class Contact(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(max_length=15, validators=[phone_regex], blank=False) # validators should be a list
     message = models.TextField()
+
+    def __str__(self):
+        return self.name
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.core.mail import EmailMessage
 from django.utils import timezone
 from .models import Post
 from .models import Type
+from .forms import ContactForm
 
 def base(request):
     tags = Type.objects.all()
@@ -18,9 +20,27 @@ def body(request):
 def post_list(request):
     tags = Type.objects.all()
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-created_date')[:3]
-    return render(request, 'blog/post_list.html', {'posts': posts, 'tags': tags})
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            email = EmailMessage('Mensagem de contato do site ' + contact.name,
+            'Nome: '+ contact.name +
+            '\n Telefone: '+ contact.phone_number +
+            '\n E-mail: ' + contact.email +
+            '\n Mensagem: ' + contact.message, to=[contact.email])
+            email.send()
+            contact.save()
+            return redirect('root_url')
+    else:
+        form = ContactForm()
+    return render(request, 'blog/index.html', {'posts': posts, 'tags': tags, 'form': form })
 
 def post_by_type(request, pk):
     type = Type.objects.get(pk=pk)
     posts = get_list_or_404(Post, type_post=type)
     return render(request, 'blog/post_filter.html', {'posts': posts, 'tag': type })
+
+def contact(request):
+    form = ContactForm()
+    return render(request, 'blog/contact.html', {'form': form })
